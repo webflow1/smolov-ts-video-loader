9f8e39d22695b8b33e445051923c1abacce30242
smoloff <smoloff@gmail.com>
2021-04-11 12:32:56 +0300

    door ts enum added

--- src/Door.ts
+++ src/Door.ts
@@ -1,43 +1,65 @@
+enum Color {
+    Red = 0,
+    Blue = 1,
+    Yellow = 2,
+}
+
+Object.keys(Color);
+
+enum LockType {
+    Key = "key",
+    Card = "card",
+    Retina = "retina"
+}
+
 interface Locker {
+    type: LockType;
+
     open(): void;
     close(): void;
 }
 
 class KeyLock implements Locker {
+    type: LockType = LockType.Key;
+
     close(): void {
         console.log("close");
     }
 
     open(): void {
-        console.log("open with key")
+        console.log(`open with ${this.type}`)
     }
 }
 
 class CardLock implements Locker {
+    type: LockType = LockType.Card;
+
     close(): void {
         console.log("close");
     }
 
     open(): void {
-        console.log("open with card")
+        console.log(`open with ${this.type}`);
     }
 }
 
 class RetinaLock implements Locker {
+    type: LockType = LockType.Retina;
+
     close(): void {
         console.log("close");
     }
 
     open(): void {
-        console.log("open with retina")
+        console.log(`open with ${this.type}`);
     }
 }
 
 class Door {
     private lock: Locker;
 
-    constructor(lock: Locker) {
-        this.lock = lock;
+    constructor(locktype: LockType) {
+        this.lock = LockFactory.create(locktype);
     }
 
     open() {
@@ -49,8 +71,23 @@ class Door {
     }
 }
 
+//Factory pattern
+class LockFactory {
+    public static create(locktype: LockType): Locker {
+        switch (locktype) {
+            case LockType.Key:
+                return new KeyLock();
+            case LockType.Retina:
+                return new RetinaLock();
+            case LockType.Card:
+                return new CardLock();
+            default:
+                throw new Error("underfined type");
+        }
+    }
+}
 
-let door1 = new Door(new RetinaLock());
+let door1 = new Door (LockType.Retina);
 door1.open();
 
 let keyLock = new KeyLock();
