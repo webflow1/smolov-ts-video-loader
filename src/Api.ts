594f628752f795e33fd71fb3888a180591190ece
Pavlo <E:\the_bat>
2021-04-14 15:20:19 +0300

    interface understanding

--- src/Api.ts
+++ src/Api.ts
@@ -31,11 +31,74 @@ export interface IApi {
     getVideoAPIData<T>(request: Request): Promise<HttpResponse<T>>;
 }
 
-export class Api implements IApi{
+export class Api implements IApi {
     public async getVideoAPIData<T>(request: Request): Promise<HttpResponse<T>> {
         return HttpApi.request(request)
     }
 }
 
 // const PexelsApi = new Api();
+const VideoJson = [{
+    per_page: 1,
+    videos: [
+        {
+            image: "somename1",
+            video_files: [
+                {
+                    link: "somelink1920",
+                    width: 1920;
+                    height: 1080;
+                },
+                {
+                    link: "somelink1280",
+                    width: 1280;
+                    height: 720;
+                }
+            ]
+        },
+        {
+            image: "somename2",
+            video_files: [
+                {
+                    link: "somelink1920",
+                    width: 1920;
+                    height: 1080;
+                },
+                {
+                    link: "somelink1280",
+                    width: 1280;
+                    height: 720;
+                }
+            ]
+        }
+    ]
+}]
 
+// const someJSON = [{
+//     name: "Ivan",
+//     age: 25,
+//     education: {
+//         base: "school",
+//         high: {
+//             1: "some1",
+//             2: "some2",
+//         },
+//     },
+// }
+// ]
+
+// interface Person {
+//     name: string
+//     age: number
+//     education: Education
+// }
+// interface Education {
+//     base: string
+//     hight: Univercity
+// }
+
+// interface Univercity {
+//     1: string
+//     2: string
+
+// }
